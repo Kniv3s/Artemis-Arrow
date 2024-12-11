679442024cb4c46c451a4dbab9ade94c8825c7b4
Kniv3s <52975761+Kniv3s@users.noreply.github.com>
2024-12-11 11:59:40 -0600

    Update artemis-arrow-create-service.ps1

--- artemis-arrow-create-service.ps1
+++ artemis-arrow-create-service.ps1
@@ -35,4 +35,4 @@ Try {
 }
 Catch { Write-Host -f red "An error occured setting the service to delayed start." }
 
-netsh interface ipv4 set subinterface "$((get-netipaddress | ? {$_.IpAddress -match "10.10"}).InterfaceAlias)" mtu=1600 store=persistent
+netsh interface ipv4 set subinterface "$((get-netipaddress | ? {$_.IpAddress -match 10.10}).InterfaceAlias)" mtu=1600 store=persistent
