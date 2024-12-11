c563a47fc16bd83e6e1c13f3033801e7106c8371
Kniv3s <52975761+Kniv3s@users.noreply.github.com>
2024-12-11 12:02:29 -0600

    Update artemis-arrow-create-service.ps1

--- artemis-arrow-create-service.ps1
+++ artemis-arrow-create-service.ps1
@@ -1,3 +1,4 @@
+#Copy all items from zip to specific directory
 $sourcePath = @( Get-Item .\dist ).FullName
 $destPath = "C:\Program Files (x86)\Artemis Arrow"
 $fileList = @(Get-ChildItem -Path .\dist -File -Recurse)
@@ -14,25 +15,25 @@ ForEach($file in $fileList){
         return
     }
 }
-
+#see if the npac drivers exist, if not install
 if (-not (Test-Path "C:\Windows\System32\drivers\npcap.sys") ){
 	.\npcap-1.80.exe
 }
-
+#if a service already exists, delete it
 if (Get-Service ArtemisArrow -ErrorAction SilentlyContinue) {
   $service = Get-WmiObject -Class Win32_Service -Filter "name='ArtemisArrow'"
   $service.StopService()
   Start-Sleep -s 1
   $service.delete()
 }
-
+#make a new service
 New-Service -name ArtemisArrow `
   -displayName "ArtemisArrow" `
   -binaryPathName "`"C:\Program Files (x86)\Artemis Arrow\ArtemisArrow.exe`""
-
+#register the service to delayed start to make persistent
 Try {
   Start-Process -FilePath sc.exe -ArgumentList 'config ArtemisArrow start= delayed-auto'
 }
 Catch { Write-Host -f red "An error occured setting the service to delayed start." }
-
+#change MTU size of outbound interface in order to account for extra bytes from VXLAN encapsulation
 netsh interface ipv4 set subinterface "$((get-netipaddress | ? {$_.IpAddress -match 10.10}).InterfaceAlias)" mtu=1600 store=persistent
