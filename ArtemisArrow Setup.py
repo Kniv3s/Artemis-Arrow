61726e81c37701cafcede42d3750db39b28aef10
Kniv3s <52975761+Kniv3s@users.noreply.github.com>
2024-12-19 07:47:51 -0600

    Update ArtemisArrow Setup.py

--- ArtemisArrow Setup.py
+++ ArtemisArrow Setup.py
@@ -10,7 +10,6 @@ setup(
     windows = [{'script': "ArtemisArrow.py"}],
     zipfile = None,
 )
-shutil.copyfile('C:\\Program Files (x86)\\Artemis Arrow\\conf.yaml', 'dist\\conf.yaml')
 os.makedirs("dist\\installers", exist_ok=True)
 shutil.copyfile('npcap-1.80.exe', 'dist\\installers\\npcap-1.80.exe')
 shutil.copyfile('artemis-arrow-create-service.ps1', 'dist\\artemis-arrow-create-service.ps1')
